09b0764256a939192595d91dc95f265ca1375cc3
Chams123456 <60320687+Chams123456@users.noreply.github.com>
2020-04-29 04:39:43 +0800

    Add files via upload

---
+++ DHT11_data_logger_test.ino
@@ -0,0 +1,35 @@
+//Include DHT Library
+#include <dht.h>
+
+//Initialise DHT Library and define data pin
+dht DHT;
+#define DHT11_PIN 4 
+
+//Variables to store temp and hum data
+int temp;
+int hum;
+
+
+void setup()
+{
+  Serial.begin(9600);  
+}
+
+void loop()
+{
+    int chk = DHT.read11(DHT11_PIN);
+    
+    temp = DHT.temperature;
+    hum = DHT.humidity;
+
+    //Print temp and hum values onto serial monitor
+    //x is used as a delimiter
+    Serial.print(temp);
+    Serial.print("x");
+    Serial.println(hum);
+    delay(2000);  
+}
+
+
+  
+  
